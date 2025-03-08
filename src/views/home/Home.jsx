7f8a1cb8bac7029e2d1e3b9e2723365810a4e713
Celia Haro <haro.celia@hotmail.fr>
2025-03-08 16:39:02 +0100

    feat: add home first part + styles

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,8 +1,15 @@
-export default function Home() {
+import styles from "./home.module.scss";
 
+export default function Home() {
     return (
-        <>
-            <p>je suis le composant Home</p>
-        </>
-    )
+        <div className={styles.hero}>
+            <section className={styles.heroContent}>
+                <h2 className="sr-only">Promoted Content</h2>
+                <p className={styles.subtitle}>No fees.</p>
+                <p className={styles.subtitle}>No minimum deposit.</p>
+                <p className={styles.subtitle}>High interest rates.</p>
+                <p className={styles.text}>Open a savings account with Argent Bank today!</p>
+            </section>
+        </div>
+    );
 }
