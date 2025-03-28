d834834865a0856e064960f06cf9778c2761533e
Celia Haro <haro.celia@hotmail.fr>
2025-03-28 16:48:59 +0100

    feat: add redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from "react-redux";
-import store from "./redux/store";
-import { router } from './router'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from "./redux/store";
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
-  </Provider>,
-)
+    <PersistGate loading={null} persistor={persistor}>
+      <StrictMode>
+        <RouterProvider router={router} />
+      </StrictMode>
+    </PersistGate>
+  </Provider>
+);
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
 import authReducer from "./authSlice";
 
-const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    },
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    // Ajout d'autres reducers ici ( comptes transactions)
 });
 
-export default store;
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['auth'],
+};
+
+// Création du reducer persistant
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configuration du store
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false, // utile pour éviter des warnings avec redux-persist
+        }),
+});
+
+// Création du persistor pour persister le store
+const persistor = persistStore(store);
+
+// Export du store et du persistor
+export { store, persistor };
